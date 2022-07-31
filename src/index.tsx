a3ad53ec56ff01465a0317e10fb0b2741b5d2463
Khrulenko Vitalii <khrulenko.v@gmail.com>
2022-07-31 22:42:45 +0300

    fixed React 18 error

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,17 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { ChakraProvider } from '@chakra-ui/react';
 import mainTheme from './styles/mainTheme';
 import store from './data/store';
 import App from './App';
 
-const Root = () => (
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(
   <Provider store={store}>
     <ChakraProvider theme={mainTheme}>
       <App />
     </ChakraProvider>
   </Provider>
 );
-
-ReactDOM.render(<Root />, document.getElementById('root'));
